74efa0611bb7b80c647120799b4f09cd7415fcda
nino <gdabdoub3@gmail.com>
2017-08-30 23:12:31 -0700

    played with log

--- scrap/main.go
+++ scrap/main.go
@@ -7,10 +7,17 @@ import (
 )
 
 func main() {
+	playingWithLogs()
+}
+
+
+func playingWithLogs() {
 	x := 1
 	log.SetLevel(log.DebugLevel)
 	l := log.WithFields(log.Fields{"x": x})
 	l.Debug("hello")
 	time.Sleep(1 * time.Second)
+	y := 2
+	l = l.WithField("y", y)
 	l.Debug("hello")
 }
